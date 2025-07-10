230f7d5cbf0810e796ecaa787fdb50aef8a38a2f
Manish <manojchourdhri@gmail.com>
2025-07-10 19:00:23 +0530

    Add files via upload

---
+++ Second_task.py
@@ -0,0 +1,37 @@
+import random,string
+
+def password_generator(length):
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    digits = string.digits
+    symbols = "@#$%&"
+
+    
+    characters = lowercase + uppercase + digits + symbols
+
+    
+    password = [
+        random.choice(lowercase),
+        random.choice(uppercase),
+        random.choice(digits),
+        random.choice(symbols)
+    ]
+
+    
+    if length < 4:
+        print("The length of the password should be 4.")
+        return None
+
+    password += random.choices(characters, k=length - 4)
+    
+    random.shuffle(password)
+    return ''.join(password)
+
+
+try:
+    length = int(input("Enter the length of the password: "))
+    result = password_generator(length)
+    if result:
+        print("Generated Password:", result)
+except ValueError:
+    print("Enter a valid number.")
